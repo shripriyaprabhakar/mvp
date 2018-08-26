0fbd6af8a20cd989e3918e7b4f53569025eb37e6
priya suresh <priyasuresh@MacBook-Pro-2.local>
2018-08-26 16:41:53 -0700

    trying to push- part-1 complete

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import config from './config.js';
 
-const API_KEY = '88204c19252c861c9eb6eaedbafc84e1';
+const API_KEY = config.API_KEY;
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
@@ -9,6 +10,7 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
   
+  console.log('req', request);
   return {
   	type: FETCH_WEATHER, 
   	payload: request
